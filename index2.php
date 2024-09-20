0c7517d64357b8d6a4efff14d391f9cf44e395cd
zaralea <hazelhern99@gmail.com>
2024-09-20 14:46:13 -0500

    v3

---
+++ index2.php
@@ -0,0 +1,26 @@
+h
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>hola html</p>
+    <?php
+    echo 'hola php';
+    echo '<h1>hola php</h1>'
+    echo '<hr>'
+    echo '<br>'
+    echo '<img src="" alt="">'
+    //comentario
+    $minombre='hazel'
+    echo 'Mi nombre es:'.$minombre;
+    $direccion='hazel.perez79@unach.mx'
+    echo 'mi direccion es'.$direccion;
+    $minumero='9613481051'
+    echo 'mi numero es'.$minumero;
+    ?>
+</body>
+</html>
